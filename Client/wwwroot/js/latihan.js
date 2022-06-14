0b149d5d74a8ce6db00ffc9de59717244c408624
yuda wirawan <yudawirawan7@gmail.com>
2022-06-14 12:53:48 +0700

    add modal to show pokemon details

---
+++ Client/wwwroot/js/latihan.js
@@ -0,0 +1,110 @@
+﻿console.log("tes latihan");
+
+var a = 1;
+console.log(a);
+
+let array = [1, 2, 3, 4, "push"];
+console.log(array);
+
+array.unshift("tes");
+console.log(array);
+
+array.shift();
+console.log(array);
+
+let arrayMulti = ['a', 'b', 'c', [1, 2, 3, ['hallo']], true];
+console.log(arrayMulti[3][3][0])
+
+let mhs = {
+    nama: "yuda",
+    nim: "12332",
+    jurusan: "Informatika",
+    umur: 23,
+    hobby: ["mancing", "tidur", "rebahan"],
+    isActive: true
+}
+
+
+const nilai = 70;
+console.log(nilai);
+
+console.log(mhs.hobby[2])
+
+let user = {};
+user.username = "yuda";
+user.password = "yuda";
+console.log(user);
+
+user.username = "xxx";
+key = "password";
+console.log(user[key]);
+
+const csv = "1|2|3";
+const [one, two, three] = csv.split("1");
+console.log(three)
+
+//array of object
+const animals = [
+    { name: "Garfield", species: "cat", class: { name: "mamalia" } },
+    { name: "Nemo", species: "fish", class: { name: "invertebrata" } },
+    { name: "Tom", species: "cat", class: { name: "mamalia" } },
+    { name: "Bruno", species: "fish", class: { name: "invertebrata" } },
+    { name: "Carlo", species: "cat", class: { name: "mamalia" } },
+]
+console.log(animals);
+
+let onlyCats = []
+animals.forEach(function (animal) {
+
+    if (animal.species === "cat") {
+        onlyCats.push(animal);
+    } else {
+        animal.class.name = "non mamalia";
+    }
+})
+
+console.log(onlyCats);
+console.log(animals);
+
+
+
+/*const OnlyCat = [];
+
+for (var i = 0; i < animals.length; i++) {
+    if (animals[i].species == "fish") {
+        animals[i].class.name = "non mamalia";
+    }
+    else if (animals[i].species == "cat") {
+        OnlyCat.push(animals[i]);
+    }
+}
+
+console.log(animals);
+
+console.log(OnlyCat);*/
+
+
+
+/*// loop
+for (var i = 0; i < animals.length; i++) {
+    console.log("hasil loop : ",animals[i]);
+}*/
+
+
+
+/*//Find index of specific object using findIndex method.    
+let objIndex = animals.findIndex((obj => obj.species == "fish"));
+
+// find and loop
+for (var a = 0; a < objIndex.length; a++) {
+    //console.log("hasil loop : ", animals[a]);
+//Log object to Console.
+    console.log("Before update: ", animals[a]);
+}
+
+//Update object's name property.
+animals[objIndex].class.name = "non mamalia"
+
+//Log object to console again.
+console.log("After update: ", animals[objIndex])
+*/
---
+++ Client/wwwroot/js/latihan2.js
@@ -0,0 +1,78 @@
+﻿
+/*tampilan text
+$.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/",
+}).done((result) => {
+    console.log(result.results);
+    let text = "";
+    $.each(result.results, function (key, val) {
+        //console.log(val.name);
+        text += `<li>${val.name}</li>`;
+    })
+    console.log(text);
+    $('#ListPoke').html(text);
+})*/
+
+
+$.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/",
+}).done((result) => {
+    console.log(result.results);
+    let text = "";
+    $.each(result.results, function (key, val) {
+        //console.log(val.name);
+        text += `<tr>
+                <td>${key+1}</td>
+                <td>${val.name}</td>
+                <td>${val.url}</td>
+                <td>
+                    <button type="button" onClick="detailPoke('https://pokeapi.co/api/v2/pokemon/${key + 1}')" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    <i class="far fa-eye"></i>
+                    </button>
+                </td>
+                </tr>`;
+    })
+    console.log(text);
+    $('#tablePoke').html(text);
+})
+
+/*function detailPoke(urlPoke) {
+    console.log(urlPoke);
+    $('#urlPoke').html(urlPoke);
+}*/
+
+function detailPoke(urlPoke) {
+    $.ajax({
+        url: urlPoke
+    }).done(u => {
+        $("#namePoke").text(u.name);
+        $("#imgPoke").attr("src", u.sprites.other.dream_world.front_default);
+        $("#valWeight").text(u.weight);
+        $("#valHeight").text(u.height);
+        $("#hpStat").width(u.stats[0].base_stat + "%").text(" HP: "+u.stats[0].base_stat + "%");
+        $("#attStat").width(u.stats[1].base_stat + "%").text("Attack: "+u.stats[1].base_stat + "%");
+        $("#defStat").width(u.stats[2].base_stat + "%").text("Defense: "+u.stats[2].base_stat + "%");
+        $("#specAttStat").width(u.stats[3].base_stat + "%").text("Special-Attack: "+u.stats[3].base_stat + "%");
+        $("#specDefStat").width(u.stats[4].base_stat + "%").text("Special-Defense: "+u.stats[4].base_stat + "%");
+        $("#speedStat").width(u.stats[5].base_stat + "%").text("Speed: " + u.stats[5].base_stat + "%");
+        $("#grassType").text(u.types[0].type.name);
+        $("#poisonType").text(u.types[1].type.name);
+        $("#ability1").text(u.abilities[0].ability.name);
+        //$("#ability1det").text(u.abilities[0].ability.effect_entries[0].effect);
+        $("#ability2").text(u.abilities[1].ability.name);
+        $("#species").text(u.species.name);
+        $("#mv0").text(u.moves[0].move.name);
+        $("#mv1").text(u.moves[1].move.name);
+        $("#mv2").text(u.moves[2].move.name);
+        $("#mv3").text(u.moves[3].move.name);
+        $("#mv4").text(u.moves[4].move.name);
+        $("#mv5").text(u.moves[5].move.name);
+        $("#mv6").text(u.moves[6].move.name);
+        $("#mv7").text(u.moves[7].move.name);
+    })
+}
+
+
+/*function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}*/
